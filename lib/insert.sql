e225266ab2d27e746026748c5f1868d5480327c9
bill <bill.arnold@student.moringaschool.com>
2023-12-07 21:00:07 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
+    ('Rowdy', 3, 'M', 'Black', 'Playful', true),
+    ('Tabitha', 4, 'F', 'Gray', 'Calm', true),
+    ('Sergeant Brown', 6, 'M', 'Brown', 'Strict', true),
+    ('Melissa', 2, 'F', 'White', 'Energetic', true),
+    ('Grinch', 7, 'M', 'Green', 'Mischievous', true),
+    ('Wendy', 4, 'F', 'Brown', 'Gentle', true),
+    ('unnamed', NULL, NULL, NULL, NULL, NULL);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,31 @@
+
+
+# Select all female bears and return their names and ages
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+# Select all bears with age greater than 3
+select_bears_with_age_greater_than_3 = """
+    SELECT *
+    FROM bears
+    WHERE age > 3;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+# Select the names of bears that are alive
+select_names_of_alive_bears = """
+    SELECT name
+    FROM bears
+    WHERE alive = true;
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+# Select the count of bears for each color
+select_count_of_bears_per_color = """
+    SELECT color, COUNT(*) as count
+    FROM bears
+    GROUP BY color;
 """
